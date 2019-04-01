d7bd8a907e2a6cadd55db7dd400a92af37ddacc1
Brent Vermolen <brentvermolen@hotmail.be>
2019-04-01 14:47:21 +0200

    Alle events in DB aan calender toevoegen

--- app/src/main/java/com/vermolen/uurrooster/Classes/Methodes.java
+++ app/src/main/java/com/vermolen/uurrooster/Classes/Methodes.java
@@ -1,19 +1,27 @@
 package com.vermolen.uurrooster.Classes;
 
 import android.Manifest;
+import android.annotation.SuppressLint;
 import android.app.Application;
 import android.appwidget.AppWidgetManager;
 import android.content.ComponentName;
 import android.content.ContentResolver;
+import android.content.ContentValues;
 import android.content.Context;
 import android.content.CursorLoader;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.provider.CalendarContract;
+import android.support.design.widget.Snackbar;
 import android.support.v4.app.ActivityCompat;
+import android.view.View;
+import android.widget.ProgressBar;
+import android.widget.Toast;
 
+import com.vermolen.uurrooster.Classes.Enums.Maand;
 import com.vermolen.uurrooster.R;
 import com.vermolen.uurrooster.Widget.MonthWidget;
 import com.vermolen.uurrooster.Widget.TodayWidget;
@@ -23,6 +31,7 @@ import com.vermolen.uurrooster.Widget.WeekWidget;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Created by Brent on 21/11/2017.
@@ -301,4 +310,77 @@ public class Methodes {
 
         return cur;
     }
+
+    /*public static void writeExistingWerkDataToCalendar(final Context context, final ProgressBar prgLoading){
+        //Vanaf november 2018 tot april 2019
+
+        final long cal_id = CalendarSingletons.sharedPreferencesCalendar.getLong("cal_id", -1);
+
+        if (cal_id == -1){
+            return;
+        }
+
+        new AsyncTask<Void, Integer, Void>(){
+            @Override
+            protected void onPostExecute(Void aVoid) {
+                prgLoading.setVisibility(View.GONE);
+                Toast.makeText(context, "Alle events zijn aangemaakt!", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            protected Void doInBackground(Void... voids) {
+                Maand maand = Maand.NOVEMBER;
+                int jaar = 2018;
+
+                while (maand != Maand.MAY){
+                    Map<Integer, List<String>> werkData = CalendarSingletons.getWerkData(maand, jaar);
+
+                    for (int dag : werkData.keySet()){
+                        String shift = werkData.get(dag).get(0);
+                        List<String> shiftData = CalendarSingletons.getShiften().get(shift);
+
+                        Calendar calendar = Calendar.getInstance();
+                        calendar.set(jaar, maand.getNr() - 1, dag, 0, 0, 0);
+                        long dagMillis = calendar.getTimeInMillis();
+
+                        calendar.set(jaar, maand.getNr() - 1, dag, Integer.parseInt(shiftData.get(1)), Integer.parseInt(shiftData.get(2)), 0);
+                        long startMillis = calendar.getTimeInMillis();
+
+                        calendar.set(jaar, maand.getNr() - 1, dag, Integer.parseInt(shiftData.get(3)), Integer.parseInt(shiftData.get(4)), 0);
+                        long endMillis = calendar.getTimeInMillis();
+
+                        ContentResolver cr = CalendarSingletons.contentResolver;
+                        ContentValues values = new ContentValues();
+                        values.put(CalendarContract.Events.DTSTART, startMillis);
+                        values.put(CalendarContract.Events.DTEND, endMillis);
+                        values.put(CalendarContract.Events.TITLE, shift);
+                        values.put(CalendarContract.Events.DESCRIPTION, shiftData.get(0));
+                        values.put(CalendarContract.Events.CALENDAR_ID, cal_id);
+                        values.put(CalendarContract.Events.EVENT_TIMEZONE, "Europe/Brussels");
+
+                        publishProgress(dag, maand.getNr(), jaar);
+                        @SuppressLint("MissingPermission") Uri uri = cr.insert(CalendarContract.Events.CONTENT_URI, values);
+                        CalendarSingletons.sharedPreferencesCalendar.edit().putLong(jaar + "/" + maand + "/" + dag, Long.parseLong(uri.getLastPathSegment())).commit();
+                    }
+
+                    maand = Maand.valueOf(maand.getNr() + 1);
+                    if (maand.getNr() == Maand.JANUARY.getNr()){
+                        jaar++;
+                    }
+                }
+                return null;
+            }
+
+            @Override
+            protected void onProgressUpdate(Integer... values) {
+                Toast.makeText(context, "Events worden toegevoegd (" + values[0] + "/" + values[1] + "/" + values[2] + ")", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            protected void onPreExecute() {
+                prgLoading.setVisibility(View.VISIBLE);
+                Toast.makeText(context, "Events worden toegevoegd", Toast.LENGTH_SHORT).show();
+            }
+        }.execute();
+    }*/
 }
